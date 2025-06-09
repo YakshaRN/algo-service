11ac0f09f5d23c06e2634efad1fe8e1bcb2bc434
yaksha_rn <yaksha.nandan@emint.co.in>
2025-06-09 08:34:28 +0530

    Initial commit

---
+++ src/main/kotlin/com.emint/AlgoServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.emint
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AlgoServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<AlgoServiceApplication>(*args)
+}
