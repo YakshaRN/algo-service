d9cbc928106640730dead6115b38ef4c64dc5d04
yaksha_rn <yaksha.nandan@emint.co.in>
2025-06-11 15:08:35 +0530

    -- repo query changed

--- src/main/kotlin/com.emint/repo/StrategyDetailRepo.kt
+++ src/main/kotlin/com.emint/repo/StrategyDetailRepo.kt
@@ -1,6 +1,5 @@
 package com.emint.repo
 
-import com.emint.data.StrategyDetailEntity
 import com.emint.enum.StrategyStatus
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
