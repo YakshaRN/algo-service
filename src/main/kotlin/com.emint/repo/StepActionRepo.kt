04b8e61ba1ea83aebdc8a74e3e0a6908387ae336
yaksha_rn <yaksha.nandan@emint.co.in>
2025-06-11 12:01:48 +0530

    -- repo query changed

--- src/main/kotlin/com.emint/repo/StepActionRepo.kt
+++ src/main/kotlin/com.emint/repo/StepActionRepo.kt
@@ -1,13 +1,12 @@
 package com.emint.repo
 
 import com.emint.data.StepActionEntity
-import com.emint.data.StrategyDetailEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import java.util.*
 
 @Repository
 interface StepActionRepo : JpaRepository<StepActionEntity, Long>{
-    fun findByStrategyId(strategyId: UUID): MutableList<StrategyDetailEntity>
-    fun findByStrategyIdAndStepNo(strategyId: UUID, stepNo: Int): StrategyDetailEntity?
+    fun findByStrategyId(strategyId: UUID): MutableList<StepActionEntity>
+    fun findByStrategyIdAndStepNo(strategyId: UUID, stepNo: Int): StepActionEntity?
 }
